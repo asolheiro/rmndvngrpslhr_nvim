9ee89293c67db1381fb965e0765d88030be3e012
Armando Solheiro <avgsolheiro@gmail.com>
2024-07-19 15:33:17 -0300

    Colorscheme projekt0n/github-nvim-theme dark_default

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -19,4 +19,13 @@ return {
       return opts
     end,
   },
-  { 'projekt0n/github-nvim-theme' }
+  {
+    'projekt0n/github-nvim-theme',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('github-theme').setup()
+      vim.cmd('colorscheme github_dark_default')
+    end,
+  }
+}
